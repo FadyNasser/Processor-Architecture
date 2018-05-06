ecbb1734d90ce3cb96adb2579d69f4632623d521
Bassel A. Elerian <basselakmal@gmail.com>
2018-05-06 20:53:44 +0200

    Add files via upload

---
+++ VHDL Files/Register.vhd
@@ -0,0 +1,24 @@
+Library ieee;
+Use ieee.std_logic_1164.all;
+
+Entity my_DFF is
+Generic ( n : integer := 16);
+port( Clk,Rst, En : in std_logic;
+d : in std_logic_vector(n-1 downto 0);
+q : out std_logic_vector(n-1 downto 0));
+end my_DFF;
+
+Architecture a_my_DFF of my_DFF is
+begin
+	Process (Clk,Rst)
+	begin
+		if Rst = '1' then
+			q <= (others=>'0');
+		elsif rising_edge(Clk) then
+			if En = '1' then
+				q <= d;
+			end if;
+		end if;
+	end process;
+end a_my_DFF;
+
