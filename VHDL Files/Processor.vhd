7332ee304cedc267812471cd8941e77c50b8a631
Fady Nasser <fady_nasser9@hotmail.com>
2018-05-07 00:21:08 +0200

    Pipelined Registers

--- VHDL Files/Processor.vhd
+++ VHDL Files/Processor.vhd
@@ -11,7 +11,7 @@ port(
 end Processor;
 
 Architecture Pipelined_Processor of Processor is
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --1)Components :-
 ------------------
 
@@ -94,7 +94,7 @@ OldSP : in std_logic_vector(15 downto 0);
 NewSP, PipeSP : out std_logic_vector(15 downto 0));
 End Component SPModule;
 
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --2)Signals :-
 --------------
 
@@ -140,16 +140,16 @@ signal MemToReg : std_logic;				-- 3
 signal PortWrite : std_logic;				-- 2
 signal PcWrite : std_logic;				-- 1
 signal RegWrite : std_logic; 				-- 0
-signal ID_Ex :std_logic_vector(128 downto 0);		-- 129 bits
+signal ID_Ex : std_logic_vector(128 downto 0);		-- 129 bits
 
 
 --EX/Mem Signals :-
 --------------------
---signal PortVal :std_logic_vector(15 downto 0);	-- 74:59
---signal RegData2 :std_logic_vector(15 downto 0);	-- 58:43
---signal Pc :std_logic_vector(15 downto 0);		-- 42:27
---signal Sp :std_logic_vector(15 downto 0);		-- 26:11
---signal Rdst :std_logic_vector(2 downto 0);		-- 10:8
+signal Result_PortVal : std_logic_vector(15 downto 0);	-- 74:59
+--signal RegData2 : std_logic_vector(15 downto 0);	-- 58:43
+--signal Pc : std_logic_vector(15 downto 0);		-- 42:27
+--signal Sp : std_logic_vector(15 downto 0);		-- 26:11
+--signal Rdst : std_logic_vector(2 downto 0);		-- 10:8
 --signal MemWrite : std_logic;				-- 7
 --signal SpSel : std_logic;				-- 6
 --signal CallSel : std_logic;				-- 5
@@ -158,31 +158,32 @@ signal ID_Ex :std_logic_vector(128 downto 0);		-- 129 bits
 --signal PortWrite : std_logic;				-- 2
 --signal PcWrite : std_logic;				-- 1
 --signal RegWrite : std_logic;				-- 0
-signal Ex_Mem :std_logic_vector(74 downto 0);		-- 75 bits
+signal Ex_Mem : std_logic_vector(74 downto 0);		-- 75 bits
 
 
 -- Mem/WB Signals :-
 ---------------------
---signal PortVal :std_logic_vector(15 downto 0);	-- 38:23
-signal ReadData :std_logic_vector(15 downto 0);		-- 22:7
+--signal PortVal : std_logic_vector(15 downto 0);	-- 38:23
+signal ReadData : std_logic_vector(15 downto 0);	-- 22:7
 --signal Rdst :std_logic_vector(2 downto 0);		-- 6:4
 --signal MemToReg : std_logic;				-- 3
 --signal PortWrite : std_logic;				-- 2
 --signal PcWrite : std_logic;				-- 1
 --signal RegWrite : std_logic;				-- 0
-signal Mem_WB :std_logic_vector(38 downto 0);		-- 39 bits
+signal Mem_WB : std_logic_vector(38 downto 0);		-- 39 bits
 
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --4)Registers Signals :-
----------------------
-signal R1_En, R2_En, R3_En ,R4_En: std_logic;
+------------------------
+signal R1_En, R2_En, R3_En ,R4_En : std_logic;
 signal R1_In, R1_Out : std_logic_vector(47 downto 0);
 signal R2_In, R2_Out : std_logic_vector(128 downto 0);
 signal R3_In, R3_Out : std_logic_vector(74 downto 0);
 signal R4_In, R4_Out : std_logic_vector(38 downto 0);
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --5)Stages Signals :-
 ---------------------
+
 --a)Fetch Signals :- "PC Module"
 --------------------------------
 signal PCSrc, PCFreeze : std_logic;
@@ -194,62 +195,60 @@ signal Instruction : std_logic_vector (15 downto 0);
 ----------------------
 
 
---b)Execute Signals :- 
+--c)Execute Signals :- 
+-----------------------
+
+
+--d)Memory Signals :- 
 ----------------------
 
 
---b)Memory Signals :- 
-----------------------
+--e)WriteBack Signals :- 
+-------------------------
 
 
---b)WriteBack Signals :- 
-----------------------
-
-
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 
 begin
---Initial Values 
+
+--Initial Values:-
+-------------------
+
 R1_En <= '1';
 R2_En <= '1';
 R3_En <= '1';
 R4_En <= '1';
--------------------------------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------------------------------------
 --1)Fetch :-
 -------------
 
 Pc_Module : PCModule port map(Interrupt, Reset, Clk, PCSrc, CallSel, PCFreeze, PCWrite ,RdstVal, WriteData, Flush_IfId, Instruction);
 
-Int <= Interrupt;
 Op <= Instruction(15 downto 11);
-Imm <= Imm(15 downto 0);
-Shift <= Instruction(3 downto 0);
-Rdst <= Instruction(7 downto 5);
 Rsrc <= Instruction(10 downto 8);
-Pc <= Pc(15 downto 0);
+Rdst <= Instruction(7 downto 5);
+Shift <= Instruction(3 downto 0);
+Int <= Interrupt;
+--Imm <= ;
+--Pc <= ;
 
-
-
-
-
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --IF/ID Reg :-
 ---------------
 IF_ID <= Int & Op(4 downto 0) & Imm(15 downto 0) & Shift(3 downto 0) & Rdst(2 downto 0) & Rsrc(2 downto 0) & Pc(15 downto 0);
 R1_In <= IF_ID;
 R1: my_DFF generic map(n => 48) port map(Clk, Reset, R1_En, R1_In, R1_Out);
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --2)Decode :-
 --------------
-Imm <= R1_Out(41 downto 26);
-Pc <= R1_Out(15 downto 0);
-Op <= R1_Out(46 downto 42);
-Shift <= R1_Out(25 downto 22);
-Rdst <= R1_Out(21 downto 19);
-Rsrc <= R1_Out(18 downto 16);
-
-
-
+--Int <= R1_Out(47);
+--Op <= R1_Out(46 downto 42);
+--Imm <= R1_Out(41 downto 26);
+--Shift <= R1_Out(25 downto 22);
+--Rdst <= R1_Out(21 downto 19);
+--Rsrc <= R1_Out(18 downto 16);
+--Pc <= R1_Out(15 downto 0);
 
 
 --RegData1 <= ;
@@ -276,84 +275,96 @@ Rsrc <= R1_Out(18 downto 16);
 --RegWrite <= ;
 
 
-
-
-
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --ID/EX Reg :-
 -----------------
-ID_EX <= RegData1(15 downto 0) & RegData2(15 downto 0) & Imm(15 downto 0) & Pc(15 downto 0) & Sp(15 downto 0) & PortVal(15 downto 0) & Op(4 downto 0) &  Shift(3 downto 0) & Rdst(2 downto 0) & Rsrc(2 downto 0) & Jz & Jn & Jc & Jmp & ALUSrc & ALUCin & ClrC & SetC & WriteCcr & PortSelect & MemWrite & SpSel & CallSel & MemPc & MemToReg & PortWrite & PcWrite & RegWrite;
+--ID_EX <= RegData1(15 downto 0) & RegData2(15 downto 0) & Imm(15 downto 0) & Pc(15 downto 0) & Sp(15 downto 0) & PortVal(15 downto 0) & Op(4 downto 0) &  Shift(3 downto 0) & Rdst(2 downto 0) & Rsrc(2 downto 0) & Jz & Jn & Jc & Jmp & ALUSrc & ALUCin & ClrC & SetC & WriteCcr & PortSelect & MemWrite & SpSel & CallSel & MemPc & MemToReg & PortWrite & PcWrite & RegWrite;
+ID_EX <= RegData1(15 downto 0) & RegData2(15 downto 0) & R1_Out(41 downto 26) & R1_Out(15 downto 0) & Sp(15 downto 0) & PortVal(15 downto 0) & R1_Out(46 downto 42) & R1_Out(25 downto 22) & R1_Out(21 downto 19) & R1_Out(18 downto 16) & Jz & Jn & Jc & Jmp & ALUSrc & ALUCin & ClrC & SetC & WriteCcr & PortSelect & MemWrite & SpSel & CallSel & MemPc & MemToReg & PortWrite & PcWrite & RegWrite;
 R2_In <= ID_EX;
 R2: my_DFF generic map(n => 129) port map(Clk, Reset, R2_En, R2_In, R2_Out);
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --3)Execute :-
 ---------------
-PortVal <= R2_Out(48 downto 33);
-RegData2 <= R2_Out(112 downto 97);
-Pc <= R2_Out(80 downto 65);
-Sp <= R2_Out(64 downto 49);
-Rdst <= R2_Out(23 downto 21);
-MemWrite <= R2_Out(7);
-SpSel <= R2_Out(6);
-CallSel <= R2_Out(5);
-MemPc <= R2_Out(4);
-MemToReg <= R2_Out(3);
-PortWrite <= R2_Out(2);
-PcWrite <= R2_Out(1);
-RegWrite <= R2_Out(0);
+--RegData1 <= R2_Out(128 downto 113);
+--RegData2 <= R2_Out(112 downto 97);
+--Imm <= R2_Out(96 downto 81);	
+--Pc <= R2_Out(80 downto 65);
+--Sp <= R2_Out(64 downto 49);
+--PortVal <= R2_Out(48 downto 33);
+--Op <= R2_Out(32 downto 28);
+--Shift <= R2_Out(27 downto 24);
+--Rdst <= R2_Out(23 downto 21);
+--Rsrc <= R2_Out(20 downto 18);
+--Jz <= R2_Out(17);
+--Jn <= R2_Out(16);
+--Jc <= R2_Out(14);
+--Jmp <= R2_Out(14);
+--ALUSrc <= R2_Out(13);
+--ALUCin <= R2_Out(12);
+--ClrC <= R2_Out(11);
+--SetC <= R2_Out(10);
+--WriteCcr <= R2_Out(9);
+--PortSelect <= R2_Out(8);
+--MemWrite <= R2_Out(7);
+--SpSel <= R2_Out(6);
+--CallSel <= R2_Out(5);
+--MemPc <= R2_Out(4);
+--MemToReg <= R2_Out(3);
+--PortWrite <= R2_Out(2);
+--PcWrite <= R2_Out(1);
+--RegWrite <= R2_Out(0);
 
 
+--Result_PortVal <=;
 
 
-
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --EX/Mem Reg :-
 -----------------
-Ex_Mem <= PortVal(15 downto 0) & RegData2(15 downto 0) & Pc(15 downto 0) & Sp(15 downto 0) & Rdst(2 downto 0) & MemWrite & SpSel & CallSel & MemPc & MemToReg & PortWrite & PcWrite & RegWrite;
+--Ex_Mem <= PortVal(15 downto 0) & RegData2(15 downto 0) & Pc(15 downto 0) & Sp(15 downto 0) & Rdst(2 downto 0) & MemWrite & SpSel & CallSel & MemPc & MemToReg & PortWrite & PcWrite & RegWrite;
+Ex_Mem <= Result_PortVal & R2_Out(112 downto 97) & R2_Out(80 downto 65) & R2_Out(64 downto 49) & R2_Out(23 downto 21) & R2_Out(7) & R2_Out(6) & R2_Out(5) & R2_Out(4) & R2_Out(3) & R2_Out(2) & R2_Out(1) & R2_Out(0);
 R3_In <= Ex_Mem;
 R3: my_DFF generic map(n => 75) port map(Clk, Reset, R3_En, R3_In, R3_Out);
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 --4)Memory :-
 --------------
-PortVal <= R3_Out(74 downto 59);
-RegData2 <= R3_Out(58 downto 43);
-Pc <= R3_Out(42 downto 27);
-Sp <= R3_Out(26 downto 11);
-Rdst <= R3_Out(10 downto 8);
-MemWrite <= R3_Out(7);
-SpSel <= R3_Out(6);
-CallSel <= R3_Out(5);
-MemPc <= R3_Out(4);
-MemToReg <= R3_Out(3);
-PortWrite <= R3_Out(2);
-PcWrite <= R3_Out(1);
-RegWrite <= R3_Out(0);
+--PortVal <= R3_Out(74 downto 59);
+--RegData2 <= R3_Out(58 downto 43);
+--Pc <= R3_Out(42 downto 27);
+--Sp <= R3_Out(26 downto 11);
+--Rdst <= R3_Out(10 downto 8);
+--MemWrite <= R3_Out(7);
+--SpSel <= R3_Out(6);
+--CallSel <= R3_Out(5);
+--MemPc <= R3_Out(4);
+--MemToReg <= R3_Out(3);
+--PortWrite <= R3_Out(2);
+--PcWrite <= R3_Out(1);
+--RegWrite <= R3_Out(0);
 
 
-
-
-
--------------------------------------------------------------------------------------------------------------
---Mem/WB Reg :-
------------------
-Mem_WB <= PortVal(15 downto 0) & RegData2(15 downto 0) & Rdst(2 downto 0) & MemToReg & PortWrite & PcWrite & RegWrite;
-R4_In <= Mem_WB;
-R4: my_DFF generic map(n => 39) port map(Clk, Reset, R4_En, R4_In, R4_Out);
--------------------------------------------------------------------------------------------------------------
---5)WriteBack :-
------------------
-PortVal <= R4_Out(38 downto 23);
-Rdst <= R4_Out(6 downto 4);
-MemToReg <= R4_Out(3);
-PortWrite <= R4_Out(2);
-PcWrite <= R4_Out(1);
-RegWrite <= R4_Out(0);
 --ReadData <= ;
 
 
+----------------------------------------------------------------------------------------------------------------------------------
+--Mem/WB Reg :-
+-----------------
+--Mem_WB <= PortVal(15 downto 0) & ReadData(15 downto 0) & Rdst(2 downto 0) & MemToReg & PortWrite & PcWrite & RegWrite;
+Mem_WB <= R3_Out(74 downto 59) & ReadData & R3_Out(10 downto 8) & R3_Out(3) & R3_Out(2) & R3_Out(1) & R3_Out(0);
+R4_In <= Mem_WB;
+R4: my_DFF generic map(n => 39) port map(Clk, Reset, R4_En, R4_In, R4_Out);
+----------------------------------------------------------------------------------------------------------------------------------
+--5)WriteBack :-
+-----------------
+--PortVal <= R4_Out(38 downto 23);
+--ReadData <= R4_Out(22 downto 7);
+--Rdst <= R4_Out(6 downto 4);
+--MemToReg <= R4_Out(3);
+--PortWrite <= R4_Out(2);
+--PcWrite <= R4_Out(1);
+--RegWrite <= R4_Out(0);
 
 
 
--------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 end Architecture Pipelined_Processor;
-
