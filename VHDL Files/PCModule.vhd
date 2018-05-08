9fa26f9b1118f8034eb1260bbaff61a24d68dd59
Bassel A. Elerian <basselakmal@gmail.com>
2018-05-08 19:09:56 +0200

    Update PCModule.vhd

--- VHDL Files/PCModule.vhd
+++ VHDL Files/PCModule.vhd
@@ -5,7 +5,7 @@ entity PCModule is
 port( 	Interrupt, Reset, Clk, PCSrc, CallSel, PCFreeze, PCWrite : in std_logic;
 	RdstVal, WriteData : in std_logic_vector(15 downto 0);
 	Flush_IfId : out std_logic;
-	Instruction, PC : out std_logic_vector (15 downto 0));
+	Instruction, PC, Imm : out std_logic_vector (15 downto 0));
 end PCModule;
 
 Architecture PCModule_Arch of PCModule is
@@ -24,8 +24,8 @@ port(A, B : in std_logic_vector(n-1 downto 0); Cin : in std_logic; F : out std_l
 end component FA;
 
 
-	signal PCSig, NewPCSig, Instr : std_logic_vector(15 downto 0);
-	signal IsInterrupt, IsReset, FA_Cout, Flush : std_logic;
+	signal PCSig, NewPCSig, Instr, InstrBuffer : std_logic_vector(15 downto 0);
+	signal IsInterrupt, IsReset, IsImmediate, FA_Cout, Flush : std_logic;
 Begin	
 	InstrMem: Memory port map(Clk, '0', x"0000", Instr, PCSig(9 downto 0));
 	PCSig_Adder: FA generic map(n => 16) port map(x"0000", PCSig, '1', NewPCSig, FA_Cout);
@@ -40,6 +40,7 @@ Begin
 			PC <= x"0000";
 			PCSig <= x"0000";
 			IsReset <= '1';
+			
 		elsif IsReset = '1' then
 			PC <= Instr;
 			PCSig <= Instr;
@@ -68,7 +69,7 @@ Begin
 						if PCSrc = '0' then
 							PC <= NewPCSig;
 							PCSig <= NewPCSig;
-							
+						
 						else
 							PC <= RdstVal;
 							PCSig <= RdstVal;							
@@ -82,9 +83,25 @@ Begin
 		end if;
 		
 	end process;
+	process(Clk) is
+	begin
+		if Reset = '1' then
+			IsImmediate <= '0';
+		elsif rising_edge(Clk) then
+			if IsImmediate = '0' then
+				if Instr(15 downto 11) = "11011" or Instr(15 downto 11) = "11100" or Instr(15 downto 11) = "11101" then
+					IsImmediate <= '1';
+					InstrBuffer <= Instr;
+				end if;
+			else
+				IsImmediate <= '0';
+			end if; 
+		end if;
+	end process;
 
-	Flush_IfId <= '1' when Flush = '1' or CallSel = '1' or PCSrc = '1' else
+	Flush_IfId <= '1' when Flush = '1' or CallSel = '1' or PCSrc = '1' or Instr(15 downto 11) = "11011" or Instr(15 downto 11) = "11100" or Instr(15 downto 11) = "11101" else
 		      '0'; 
-	Instruction <= Instr;
+	Imm <= Instr when IsImmediate = '1' else x"0000";
+	Instruction <= InstrBuffer when IsImmediate = '1' else Instr;
 
 End Architecture PCModule_Arch;
