51dff51d430dd92cbe9c549f2f56fe0ed1577ed7
Bassel A. Elerian <basselakmal@gmail.com>
2018-05-10 01:06:35 +0200

    Update ControlUnit.vhd

--- VHDL Files/ControlUnit.vhd
+++ VHDL Files/ControlUnit.vhd
@@ -6,7 +6,7 @@ Port(
 OPCode	: in std_logic_vector(4 downto 0);
 Int	: in std_logic;
 JZ, JN, JC, JMP, ALUSrc, ALUCin, SETC, CLRC, MemWrite, MemRead, RegWrite, MemToReg, PortSelect, PortWrite, SPSel,
-PCWrite, MemPC, CallSel, WriteCCR, ReadImm: out std_logic);
+PCWrite, MemPC, CallSel, WriteCCR, ReadImm, RTI_Flush: out std_logic);
 End Entity CU;
 
 Architecture CU_Arch of CU is
@@ -27,8 +27,10 @@ Begin
 	MemWrite   <= '1' when OP = "01100" or OP = "11000" or OP = "11101" or Int = '1' else '0';
 	MemRead    <= '1' when OP = "01101" or OP = "11001" or OP = "11010" or OP = "11100" else '0';
 
-	RegWrite   <= '0' when OP = "00000" or OP = "01010" or OP = "01011" or OP = "01100" or OP = "01110" or OP = "10100" or OP = "10101" or
-			     OP = "10110" or OP = "10111" or OP = "11000" or OP = "11001" or OP = "11010" or OP = "11101" or Int = '1' else '1';
+	RegWrite   <= '1' when OP = "00001" or OP = "00010" or OP = "00011" or OP = "00100" or OP = "00101" or OP = "00110" or OP = "00111" or OP = "01000" or
+			       OP = "01001" or OP = "01101" or OP = "01111" or OP = "10000" or OP = "10001" or OP = "10010" or OP = "10011" or OP = "11011" or
+			       OP = "11100" else '0';
+
 	MemToReg   <= '0' when OP = "00000" or OP = "01101" or OP = "11001" or OP = "11010" or OP = "11100" or Int='1' else '1';
 	
 	PortSelect <= '1' when OP = "01111" else '0';
@@ -36,7 +38,7 @@ Begin
 
 	SPSel	   <= '1' when OP = "01100" or OP = "01101" or OP = "11000" or OP = "11001" or OP = "11010" or Int = '1' else '0';
 
-	PCWrite	   <= '1' when OP = "11000" or OP = "11001" or OP = "11010" or Int = '1' else '0';
+	PCWrite	   <= '1' when OP = "11000" or OP = "11001" or OP = "11010" else '0';
 
 	MemPC	   <= '1' when OP = "11000" or Int = '1' else '0';
 	
@@ -45,5 +47,6 @@ Begin
 	WriteCCR   <= '1' when OP = "11010" else '0';
 
 	ReadImm	   <= '1' when OP = "11011" or OP = "11100" or OP = "11101" else '0';
-
+	
+	RTI_Flush  <= '1' when OP = "11010" else '0';
 End Architecture CU_Arch;
